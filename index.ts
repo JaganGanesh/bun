afb249d063fee4a4325e279612a48542cf75d398
Jagan Ganesh <heyitsjagan@icloud.com>
2023-09-22 19:43:33 +0530

    Server using Bun.serve

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  port: 8080,
+  fetch(data) {
+    return new Response("Hello World!");
+  },
+});
+
+console.log(`Listening on port ${server.port}`);
