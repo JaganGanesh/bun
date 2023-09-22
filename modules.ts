c6518c645563c43600584858f573b041e2a553ae
Jagan Ganesh <heyitsjagan@icloud.com>
2023-09-22 20:11:48 +0530

    Node Core Modules

---
+++ modules.ts
@@ -0,0 +1,7 @@
+import path from "path";
+// const path = require("path");
+
+const filePath = path.join("foo", "bar", "image.png");
+const fileName = path.basename(filePath);
+
+console.log(fileName);
