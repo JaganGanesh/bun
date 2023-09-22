e2508f3a1c5ad76fe1398984e3050e8f24d955cc
Jagan Ganesh <heyitsjagan@icloud.com>
2023-09-22 20:27:09 +0530

    Testing

---
+++ index.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, test, beforeAll } from "bun:test";
+
+beforeAll(() => {
+  // Setup Test
+});
+
+// Test
+describe("Math", () => {
+  // Addition
+  test("Addition", () => {
+    expect(2 + 2).toBe(4);
+  });
+});
